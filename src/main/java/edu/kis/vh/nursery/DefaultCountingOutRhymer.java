f56f91d4645bc0d099a377386066294a7814ae5c
AbRa734 <sz.o@yahoo.com>
2024-05-08 19:12:38 +0200

    6 7 8 stale i adnotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,35 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int ARRAY_SIZE = 12;
+    public static final int DEFAULT_NEGATIVE_VALUE = -1;
+    private int[] NUMBERS = new int[ARRAY_SIZE];
 
     public int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            NUMBERS[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == DEFAULT_NEGATIVE_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == (ARRAY_SIZE+DEFAULT_NEGATIVE_VALUE);
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
-        return numbers[total];
+            return DEFAULT_NEGATIVE_VALUE;
+        return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
-        return numbers[total--];
+            return DEFAULT_NEGATIVE_VALUE;
+        return NUMBERS[total--];
     }
 
 }
